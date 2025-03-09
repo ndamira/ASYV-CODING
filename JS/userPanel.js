051acdabbbfe788b3ca25804b666fd9339aa3fb9
Regis <regis.ndamira@asyv.org>
2025-03-09 14:18:33 +0200

    Notifications || User Panel

---
+++ JS/userPanel.js
@@ -0,0 +1,9 @@
+let notice = document.getElementById("notice");
+
+function openNotification(){
+    notice.classList.toggle("openNotification");
+}
+
+function closeNotification(){
+    notice.classList.remove("openNotification");
+}
