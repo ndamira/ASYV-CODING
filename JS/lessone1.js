d0692b0a7d9d943189545f54602803f36395b9c7
Regis <96113923+ndamira@users.noreply.github.com>
2025-03-12 17:51:40 +0200

    Lesson dashboard || User Panel

---
+++ JS/lessone1.js
@@ -0,0 +1,67 @@
+let notice = document.getElementById("notice");
+let logout = document.getElementById("logout");
+let course = document.getElementsByClassName("course");
+let courseDetails = document.getElementsByClassName("course-details");
+let cancelBtn = document.getElementsByClassName("btn");
+let lesson = Array.from(document.getElementsByClassName("lesson"));
+let lessonContent = document.getElementsByClassName("information");
+
+function openNotification(){
+    logout.classList.remove("openLogout");
+    notice.classList.toggle("openNotification");
+}
+
+function closeNotification(){
+    notice.classList.remove("openNotification");
+}
+
+function openLogout(){
+    notice.classList.remove("openNotification");
+    logout.classList.toggle("openLogout");
+}
+
+function closeLogout(){
+    logout.classList.remove("openLogout");
+}
+
+function searchCourse(){
+    let input = document.getElementById("search");
+    let upperCase = input.value.toUpperCase();
+    let allCourses = document.getElementById("allCourses");
+    let course = document.getElementsByClassName("course");
+    for( let i = 0; i < course.length; i++){
+        let courseTitle = course[i].getElementsByTagName("h3")[0];
+        let title = courseTitle.textContent || courseTitle.innerText;
+        if(title.toUpperCase().indexOf(upperCase) > -1){
+            course[i].style.display = "";
+        }
+        else{
+            course[i].style.display = "none";
+        }
+    }
+}
+
+for(let i = 0; i < course.length; i++){
+    course[i].addEventListener("click", () =>{
+        courseDetails[i].classList.add("open-popup");
+    })
+}
+
+for(let i = 0; i < cancelBtn.length; i++){
+    cancelBtn[i].addEventListener("click", () =>{
+        courseDetails[i].classList.remove("open-popup");
+        console.log(courseDetails[i]);
+    })
+}
+
+for (let i = 0; i < lesson.length; i++) {
+    lesson[i].addEventListener("click", () => {
+        for (let j = 0; j < lesson.length; j++) {
+            lesson[j].classList.remove("active");
+            lessonContent[j].classList.add("hide-content");
+        }
+
+        lesson[i].classList.add("active");
+        lessonContent[i].classList.remove("hide-content");
+    });
+}
