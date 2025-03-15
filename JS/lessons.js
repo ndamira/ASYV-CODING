3434d1162b0881c7688e35d0b012e89953437015
Regis <96113923+ndamira@users.noreply.github.com>
2025-03-15 21:31:58 +0200

    Admin Panel

---
+++ JS/lessons.js
@@ -0,0 +1,45 @@
+let toggleButton = document.getElementById("toggle-btn");
+let sidebar = document.getElementById("sidebar");
+
+function toggleSidebar() {
+  sidebar.classList.toggle("close");
+  toggleButton.classList.toggle("rotate");
+  Array.from(sidebar.getElementsByClassName("show")).forEach((ul) => {
+    ul.classList.remove("show");
+    ul.previousElementSibling.classList.remove("rotate");
+  });
+}
+
+function toggleSubMenu(button) {
+  button.nextElementSibling.classList.toggle("show");
+  button.classList.toggle("rotate");
+
+  if (sidebar.classList.contains("close")) {
+    sidebar.classList.toggle("close");
+    toggleButton.classList.toggle("rotate");
+  }
+}
+
+function deleteUser() {
+  document.getElementById("delete").classList.add("show-delete");
+}
+
+function closeDelete() {
+  document.getElementById("delete").classList.remove("show-delete");
+}
+
+function approveUser() {
+  document.getElementById("approve").classList.add("show-approve");
+}
+
+function closeApprove() {
+  document.getElementById("approve").classList.remove("show-approve");
+}
+
+function addLesson() {
+  document.getElementById("addLesson").classList.add("addLesson");
+}
+
+function cancelLesson() {
+  document.getElementById("addLesson").classList.remove("addLesson");
+}
